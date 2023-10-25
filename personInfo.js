b31b73e8bf7f15db6e1aadf89a73b2eb0b5a82ea
GalaSport <146236308+GalaSport@users.noreply.github.com>
2023-10-25 23:17:42 +0300

    Add files via upload

---
+++ personInfo.js
@@ -0,0 +1,16 @@
+
+function personInfo(firstName, lastName, age){
+let person = {
+    firstName:firstName,
+    lastName: lastName,
+    age: age
+
+};
+    return person;
+    
+}   
+let result = personInfo("Peter", "Pan", 20);
+console.log(result);
+    
+
+    

dc74089e0d43c0f6c6a3df6678e71848662f12c1
GalaSport <146236308+GalaSport@users.noreply.github.com>
2023-10-28 18:50:29 +0300

    Add files via upload

---
+++ classDemo.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age){
+        //let this = {};
+        this.name = name;
+        this.age = age;
+        //return this;
+    }
+}
+let myPerson = new Person("Peter", 23);
+console.log(myPerson);

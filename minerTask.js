8669152d3f05ef8b2566394374898162edeed63a
GalaSport <146236308+GalaSport@users.noreply.github.com>
2023-11-13 19:33:56 +0200

    Add files via upload

---
+++ minerTask.js
@@ -0,0 +1,17 @@
+function minerTask(array){
+    let resources = {};
+    for (let i = 0; i < array.length; i += 2) {
+        let resource = array[i];
+        let qty = Number(array[i + 1]);
+        if(resource in resources){
+            resources[resource] += qty;
+        }else{
+            resources[resource] = qty;
+        }   
+    }
+    let entries = Object.entries(resources);
+    for(let [resource, qty] of entries){
+        console.log(`${resource} –> ${qty}`);
+    }
+}
+minerTask(['Gold', '155', 'Silver', '10', 'Copper', '17']);
